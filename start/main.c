aac9c27ea846686e201929e09cd32799f8f40979
JeongHwan <jhlee51047@naver.com>
2017-11-10 06:13:17 -0800

    Add Manual to main.c

--- start/main.c
+++ start/main.c
@@ -6,5 +6,18 @@ void main()
   printf("Choose Game!!\n");
   printf("1. BBopgi Game      2. Rock Scissors Paper\n");
   
-  scanf("%d",&a);
+  while(1){
+    scanf("%d",&a);
+    if( a == 1){
+      //bbopgi();
+      break;
+    }
+    else if( a == 2){
+      //RSP();
+      break;
+    }
+    else{
+      printf("Please Choose 1~2\n");
+    }
+  }
 }
