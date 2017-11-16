860e8862125f87aff5ac48848528b93ce0ecf60a
JeongHwan <jhlee51047@naver.com>
2017-11-15 18:05:29 -0800

    add card

--- bbopgi/bbopgi.c
+++ bbopgi/bbopgi.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-//z
+
 void main()
 {
-  int a;
-  printf("How many people are you going to play?\n")
+  char c[100];
+  int a,b;
+  printf("How many people are you going to play?\n");
   scanf("%d",&a);
+  printf("How many 'Bomb' you needs?\n");
+  scanf("%d",&b);
+  
+  for( int i = 0 ; i < a; i++){
+    c[i] = 0; // 0 is pass 1 is fail
+  }
 
+  for( int i = 0 ; i < b; i++){
+    c[i] = 1;
+  }  
 }
