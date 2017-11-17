e63234439183c6471b13cffc30f2149eb7d6c9c7
JeongHwan <jhlee51047@naver.com>
2017-11-17 04:46:42 -0800

    add random index

--- bbopgi/bbopgi.c
+++ bbopgi/bbopgi.c
@@ -17,5 +17,31 @@ void main()
 
   for( int i = 0 ; i < b; i++){
     c[i] = 1;
-  }  
+  }
+  
+  
+}
+
+void shuffle(int aa, char *bb)
+{
+  srand((unsigned int)time(NULL));
+  
+  char e,f;
+  char d[100];
+  for(int i = 0; i < aa; i++){
+    d[i] = rand() % aa  + 1;
+	
+    for(int j = 0; j < i; j++){
+      if( d[i] == d[j] ){
+        i--;
+	break;
+      }
+    }
+  }
+  
+  
+  for(int i = 0; i < aa; i++){
+     
+  } 
+ 
 }
