d34d59436f1afd0225959e56c4668086265643aa
JeongHwan <jhlee51047@naver.com>
2017-11-18 15:09:07 -0800

    change structure

--- bbopgi/bbopgi.c
+++ bbopgi/bbopgi.c
@@ -2,33 +2,49 @@
 #include <time.h>
 #include <stdlib.h>
 
-void shuffle(int aa, int bb, char *cc);
+typedef struct
+{
+  char data;
+  char flag;
+}card;
+
+void shuffle(int aa, int bb, card *cc);
 
 void main()
 {
-  char c[100];
-  int a,b;
-  printf("How many people are you going to play?\n");
+  card c[100];
+  int a,b,g;
+  printf("How many bbopgi card  you need to play?\n");
   scanf("%d",&a);
   printf("How many 'Bomb' you needs?\n");
   scanf("%d",&b);
   
   for( int i = 0 ; i < a; i++){
-    c[i] = 0; // 0 is pass 1 is fail
+    c[i].data = 0; // 0 is pass 1 is fail
+    c[i].flag = 0;
   }
 
   for( int i = 0 ; i < b; i++){
-    c[i] = 1;
+    c[i].data = 1;
   }
   shuffle(a,b,c);
   
+ // for(int i = 0; i < a; i++){
+    printf("Press button in 1 ~ %d\n", a);
+   // scanf("%d", &g);
+    for(int j = 0; j < a; j++){
+        printf("%d :", j+1);
+     //   if( c[g-1] == 1) printf("X\n");
+       // if( c[g-1] == 0) printf("O\n");
+    }
+  //} 
 }
 
-void shuffle(int aa, int bb, char *cc)
+void shuffle(int aa, int bb, card *cc)
 {
   srand((unsigned int)time(NULL));
   
-  char e;
+  char e,f;
   char d[100];
   for(int i = 0; i < bb; i++){
     d[i] = rand() % aa  + 1;
@@ -44,8 +60,8 @@ void shuffle(int aa, int bb, char *cc)
   
   for(int i = 0; i < bb; i++){
     e = d[i];
-    f = cc[e];
-    cc[e] = cc[i];
-    cc[i] = f;
+    f = cc[e].data;
+    cc[e].data = cc[i].data;
+    cc[i].data = f;
   } 
 }
