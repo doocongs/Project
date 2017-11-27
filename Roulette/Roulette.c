e7e6da6fa0d5729c532f36cf7d3c0237da9d27fe
Wansoo <anton9375@naver.com>
2017-11-27 18:23:50 +0900

    Roulett

---
+++ Roulette/Roulette.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+
+void Roulette(){
+
+  srand(time(0));
+  int PeopleNum;
+
+  printf("How many people play??..\n");
+  scanf("%d", &PeopleNum);
+
+// struct here....
+  
+
+  printf("Choose Roulette Number...\n");
+  scanf("%d", &Num);
+ 
+
+  int DeadNum=rand()%Num;
+  
+ // ... code... 
+  if()
+  else if()
+  else
+
+
+
+
+}
