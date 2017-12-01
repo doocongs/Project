6145eea17c29a76bd91e505a6b8c69d725b5ca48
JeongHwan <jhlee51047@naver.com>
2017-12-01 02:19:36 -0800

    finish 31

--- 31/31.c
+++ 31/31.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 void 31(){
-  
+  int player;
+  int plyaer_sum = 0;
+  int num;
+  int sum = 0;
+  printf("How many players?\n");
+  scanf("%d",&player);
 
+  while(1){
+    player_sum++;
+    if(player_sum > player){
+      player_sum = 1;
+    }
+    printf("Player %d!! Enter your number 1~3\n",&player_sum);
+    scanf("%d",&num);
+    if(num > 4){
+      printf("Enter only 1~3!!!\n");
+      player_sum--;
+      num = 0;
+    }   
+    sum = sum + num;
+    if( sum >= 31 ){
+      printf("Player%d is winner!!",player_sum);
+      break;
+    }
+  }
 }
