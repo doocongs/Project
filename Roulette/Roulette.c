d8e06b5c02f97abd6ec80155eaccce2568caa212
Wansoo <anton9375@naver.com>
2017-12-02 01:13:43 +0900

    start Roulette

--- Roulette/Roulette.c
+++ Roulette/Roulette.c
@@ -2,24 +2,41 @@
 #include<time.h>
 #include<stdlib.h>
 
+struct Student
+{
+  char Name[32];
+}
+
+struct Student StudentList[Max];
+
 
 void Roulette(){
 
+
   srand(time(0));
   int PeopleNum;
+  int Rnum;
+  int RandNum;
 
   printf("How many people play??..\n");
   scanf("%d", &PeopleNum);
-
-// struct here....
   
+  for(int i=0;i<PeopleNum;i++)
+  {
+    printf("Insert your Name\n");
+    scanf("%s", StudentList[i].Name);
+  }
 
   printf("Choose Roulette Number...\n");
-  scanf("%d", &Num);
- 
-
-  int DeadNum=rand()%Num;
+  scanf("%d",&Rnum );
+  RandNum=rand()%(Rnum+1);
   
+  
+  while(1){
+  
+  
+  
+  }
  // ... code... 
   if()
   else if()
