b2c38144cf00470fb3ca8c8b525c3c76e88e71f8
Wansoo <anton9375@naver.com>
2017-12-02 01:32:48 +0900

    Roulette adit!!@@

--- Roulette/Roulette.c
+++ Roulette/Roulette.c
@@ -2,15 +2,16 @@
 #include<time.h>
 #include<stdlib.h>
 
+#define Max 100
 struct Student
 {
   char Name[32];
-}
+};
 
 struct Student StudentList[Max];
 
 
-void Roulette(){
+int main(){
 
 
   srand(time(0));
@@ -31,18 +32,46 @@ void Roulette(){
   scanf("%d",&Rnum );
   RandNum=rand()%(Rnum+1);
   
+  printf("%d is The Random Number\n", RandNum);
+
+  int LastValue =RandNum;
+
+  int k=0;
+  char N;
+
+  while(1)
+  {
+    RandNum =rand()% (Rnum +1);
+    printf("%s's turn, Press 'N'BUTTON\n",StudentList[k].Name);
+    while(1){
+    
+    scanf("%c", &N);
+    if(N=='N'){
+    break;
+    }
+    }
+
+    if(RandNum==LastValue)
+    {
+    printf("<<< %d >>> You are Lose....\n", RandNum);
+    printf("GAME OVER");
+    return 0;
+    }
+    else
+    {
+    printf("<<< %d >>> ALive....\n",RandNum);
+    k++;
+    if(k == PeopleNum)
+    {
+     k=0;
+    }
+    }
   
-  while(1){
+  
   
   
   
   }
- // ... code... 
-  if()
-  else if()
-  else
-
-
 
 
 }
